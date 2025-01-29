e9d1801852193500c7eeff9e818caba245119475
Kris Stalmokas <pg011732@student.reading.ac.uk>
2025-01-29 18:12:28 +0000

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,27 @@
+
+
+//my link functions
+
+function linkSteam () {
+    window.open("https://steamcommunity.com/id/yerdayerma");
+}
+
+function linkDiscord () {
+    window.open("https://steamcommunity.com/id/yerdayerma");
+}
+
+function linkTwitter () {
+    window.open("https://steamcommunity.com/id/yerdayerma");
+}
+
+function linkInstagram () {
+    window.open("https://steamcommunity.com/id/yerdayerma");
+}
+
+function linkSpotify () {
+    window.open("https://open.spotify.com/user/iyihsdd01qcv35yssl6w8qpas?si=287f728ac29c47f2");
+}
+
+function linkLastfm () {
+    window.open("https://steamcommunity.com/id/yerdayerma");
+}
